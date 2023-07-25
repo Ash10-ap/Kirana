030321a01f5e8ca0cf14564c1c125df6c021354b
shailesh Gore <shaileshgore1998@gmail.com>
2023-07-25 16:27:50 +0530

    home page and cart functionality(1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/HomePage/Home";
+import AddToCart from "./Components/AddToCart/AddToCart";
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<AddToCart />} />
+      </Routes>
     </div>
   );
 }
