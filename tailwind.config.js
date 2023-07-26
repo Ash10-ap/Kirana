a2703d6e1b55e23dc2c7c8b022d7b853de6321b3
Ash10-ap <atharvpatil2001@gmail.com>
2023-07-26 13:58:55 +0530

    modified files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.js",
+// export const prefix = 'tw-';
+export const content = ["./src/**/*.js",
   "./src/**/*.jsx",
   "./src/**/*.ts",
-  "./src/**/*.tsx",
-];
+  "./src/**/*.tsx",];
 export const theme = {
   extend: {},
 };
