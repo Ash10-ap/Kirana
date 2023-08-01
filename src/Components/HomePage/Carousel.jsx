91407fb994a26d8977280b6943d39a0d78bf5794
shailesh Gore <shaileshgore1998@gmail.com>
2023-08-01 14:29:49 +0530

    quantity feature update and carousel btn

--- src/Components/HomePage/Carousel.jsx
+++ src/Components/HomePage/Carousel.jsx
@@ -20,11 +20,11 @@ const Carousel = () => {
           <img src={image3} className="d-block w-100 img-fluid" alt="Image 3" style={{ height: imageHeight, width: imageWidth }}/>
         </div>
       </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <button className="carousel-control-prev bg-transparent" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <button className="carousel-control-next bg-transparent" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
--- src/store/slices/CartSlice.jsx
+++ src/store/slices/CartSlice.jsx
@@ -5,9 +5,20 @@ const cartSlice = createSlice({
   initialState: [],
   reducers: {
     addToCart(state, action) {
-      const newItem = { ...action.payload, quantity: 1 }; 
-      state.push(newItem);
-      console.log("in actions", action.payload);
+      const newItem = { ...action.payload, quantity: 1 };
+
+      if (state.length === 0) {
+        state.push(newItem);
+      } else {
+        const existingElement = state.findIndex((ele) => ele.id === newItem.id);
+        if (existingElement === -1) {
+          state.push(newItem);
+        } else {
+          state[existingElement].quantity = state[existingElement].quantity + 1;
+        }
+      }
+
+      console.log("in actions", newItem);
     },
     removeFromCart(state, action) {
       return state.filter((item) => item.id !== action.payload);
@@ -18,14 +29,14 @@ const cartSlice = createSlice({
 
     increaseTheQuantity(state, action) {
       const item = state.find((item) => item.id === action.payload.i);
-      if(item && item.quantity >= 1){
+      if (item && item.quantity >= 1) {
         item.quantity = item.quantity + 1;
       }
     },
 
     decreaseTheQuantity(state, action) {
-        const item = state.find((item) => item.id === action.payload.i);
-      if(item && item.quantity > 1){
+      const item = state.find((item) => item.id === action.payload.i);
+      if (item && item.quantity > 1) {
         item.quantity = item.quantity - 1;
       }
     },
